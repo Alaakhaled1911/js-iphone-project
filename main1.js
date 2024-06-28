a0ac30d757e4c6de676d7ac29bd06c8b7ad0d032
Your Name <you@example.com>
2024-06-29 01:59:51 +0300

    first commit

---
+++ main1.js
@@ -0,0 +1,10 @@
+let image =document.querySelector('.imgg');
+let container=document.querySelector('.container');
+function phones (phone)
+{
+    image.src=phone;
+}
+function colors (color)
+{
+    container.style.background=color;
+}
